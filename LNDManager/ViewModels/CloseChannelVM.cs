72d8236b8017cbf4543e06c578fde2929fdb5e1e
Lukaš Rada <lukas.rada@hotmail.cz>
2020-11-16 13:18:00 +0100

    Closes #2, minor bug fixes

---
+++ LNDManager/ViewModels/CloseChannelVM.cs
@@ -0,0 +1,21 @@
+﻿using Lnrpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LNDManager.ViewModels
+{
+    public class CloseChannelVM
+    {
+        public ChannelPoint ChannelPoint { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
+        public bool IsForcedClose { get; set; }
+
+        public long SatPerByte { get; set; }
+
+        public int TargetConf { get; set; }
+    }
+}
